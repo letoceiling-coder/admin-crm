d4b78505ed8a0b8d951e23a7bab7a806fec7544e
letoceiling-coder <dsc-23@yandex.ru>
2026-01-27 22:11:08 +0300

    Deploy: 2026-01-27 19:11:07

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    css: {
+        postcss: './postcss.config.cjs',
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
